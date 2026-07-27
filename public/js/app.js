e14c4442168ed227a475935217d4e6d5c68c7a6c
wirat <ood.wirat2533@gmail.com>
2026-07-27 14:33:43 +0700

    Disable lazy loading for folders on desktop devices to show all folders instantly

--- public/js/app.js
+++ public/js/app.js
@@ -286,7 +286,7 @@ function renderAlbumGrid(folders) {
   }
 
   let currentIndex = 0;
-  const batchSize = 30;
+  const batchSize = window.innerWidth > 768 ? 10000 : 30;
 
   function renderBatch() {
     const fragment = document.createDocumentFragment();
@@ -414,7 +414,7 @@ function renderSubfolderView(subfolders, directPhotosData) {
   subGrid.className = 'album-grid';
   subGrid.style.cssText = 'margin-bottom: 1.5rem; grid-column: 1/-1;';
   let currentFolderIndex = 0;
-  const folderBatchSize = 30;
+  const folderBatchSize = window.innerWidth > 768 ? 10000 : 30;
 
   function renderFolderBatch() {
     const fragment = document.createDocumentFragment();
