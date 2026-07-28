2304f1966fe8e93c9a89dd945b099245c47bc6ae
wirat <ood.wirat2533@gmail.com>
2026-07-28 21:23:19 +0700

    Fix Google Drive upload permissions scope

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -22,7 +22,7 @@ export const authOptions: AuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
       authorization: {
         params: {
-          scope: "openid email profile https://www.googleapis.com/auth/drive.file",
+          scope: "openid email profile https://www.googleapis.com/auth/drive",
           prompt: "consent",
           access_type: "offline",
           response_type: "code"
